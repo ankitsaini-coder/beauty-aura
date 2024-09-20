d97c23eee1204573ac81dc66a5477606cceadd38
ankitsaini-coder <mrankitsaini231@gmail.com>
2024-09-20 15:44:44 +0530

    fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,19 @@
 import Layout from "../components/Layout";
+// import Home_img1 from "../assets/img/aura_home_img1.webp"
+import Slider from "react-slick";
 
 
 
-const Home = () => {
 
+
+const Home = (item) => {
+  
   return (
     <Layout>
-      
     </Layout>
   );
 }
 
 export default Home;
+
+
