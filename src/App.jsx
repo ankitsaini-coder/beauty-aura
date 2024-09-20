d80edcaffc2652d4a7083f57691ee1b34fffc43c
ankitsaini-coder <mrankitsaini231@gmail.com>
2024-09-20 15:44:35 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import Navbar from "./components/Navbar";
 import Navigation from "./routes/Navigation";
+
 
 function App() {
   return (
     <>
-      {/* <Navbar /> */}
       <Navigation/>
     </>
   );
